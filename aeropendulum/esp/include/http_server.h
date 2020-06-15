1b29421e46edb516fd7ec58e814573ecf5cf432a
MarcoMiretti <marco.miretti@gmail.com>
2020-06-15 17:53:44 -0300

    :white_check_mark: add tests for json parser
    A test task and callback is added to the firmware in order to validate
    the json library.
    A tiny refactor is made to the python tests.

--- aeropendulum/esp/include/http_server.h
+++ aeropendulum/esp/include/http_server.h
@@ -13,9 +13,12 @@ enum URI_TASK
     URI_UNDEF,
 	URI_CLASSIC,
     URI_PING,
+    URI_PARSE_TEST,
 };
 
 
 void classic_controller_task(void *pvParameter);
 
 void ping_task(void *pvParameter);
+
+void test_task(void *pvParameter);
---
+++ aeropendulum/esp/test_task.c
@@ -0,0 +1,20 @@
+#include <espressif/esp_common.h>
+#include <esp8266.h>
+#include <esp/uart.h>
+#include <string.h>
+#include <stdio.h>
+#include <FreeRTOS.h>
+#include <task.h>
+#include <http_server.h>
+
+#define TEST_WAIT_TIME_ms 500
+// TODO: ugh 
+extern uint8_t URI_TASK;
+
+void test_task(void *pvParameter)
+{
+    vTaskDelay(TEST_WAIT_TIME_ms / portTICK_PERIOD_MS);
+    URI_TASK = URI_UNDEF;
+    printf("Disconected, delete task");
+    vTaskDelete(NULL);
+}
