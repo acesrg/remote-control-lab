52cf74a2f11cacca3126aacdfd03fe9c84571afc
MarcoMiretti <marco.miretti@gmail.com>
2020-07-15 08:23:17 -0300

    :sparkles: add pwm propeller commanding
    Write to the actuators in the classic controller task, to change the pwm
    duty using the values previously set in the database.
    
    To configure the pwm (pin, frequency) a new uri is added.

--- aeropendulum/esp/include/http_server.h
+++ aeropendulum/esp/include/http_server.h
@@ -8,10 +8,12 @@ enum URI_TASK
 {
     URI_UNDEF,
 	URI_CLASSIC,
+    URI_START_PWM,
     URI_PING,
     URI_PARSE_TEST,
 };
 
+void start_pwm_task(void *pvParameter);
 
 void classic_controller_task(void *pvParameter);
 
--- aeropendulum/esp/tasks/start_pwm.c
+++ aeropendulum/esp/tasks/start_pwm.c
@@ -12,41 +12,35 @@
 #include <classic_controller.h>
 #include <json_parser.h>
 #include <pwm.h>
+#include <propeller_configurations.h>
 
 // TODO: ugh such an ugly global
 extern uint8_t URI_TASK;
 
+PwmConfigType pwm_config = {
+                            DEFAULT_PROPELLER_PWM_FREQUENCY_HZ,
+                            DEFAULT_PROPELLER_PWM_POWERON_DUTY,
+                            DEFAULT_PROPELLER_PWM_PIN
+                            };
 
-void start_propeller_task(void *pvParameter){
+void start_pwm_task(void *pvParameter){
     
     uint8_t pins[1];
     log_trace("task started");
-    log_trace("pwm_init(1, [14])\n");
-    
-    pins[0] = 14;
+    log_trace("pwm_init(1, [%d])", pwm_config.pin);
+    pins[0] = pwm_config.pin;
     pwm_init(1, pins, false);
 
-    log_trace("pwm_set_freq(100)");
-    pwm_set_freq(100);
+    log_trace("pwm_set_freq %d", pwm_config.frequency_hz);
+    pwm_set_freq(pwm_config.frequency_hz);
 
     log_trace("pwm set duty");
-    pwm_set_duty(7209);
+    pwm_set_duty(pwm_config.poweron_duty);
 
     log_trace("pwm_start");
     pwm_start();
     
-    log_trace("wait 1 sec");
-    vTaskDelay(1000 / portTICK_PERIOD_MS);
-
-    for (int i = 7600; i > 7209; i -= 10) {
-        log_trace("pwm: %d",i);
-        pwm_set_duty(i);
-        vTaskDelay(500 / portTICK_PERIOD_MS);        
-    }
-    pwm_set_duty(7209);
-
     URI_TASK = URI_UNDEF;    
     log_trace("disconected, delete task");
     vTaskDelete(NULL);
-
 }
