6c230ce349780056107ada635f133aa5e689dd76
MarcoMiretti <marco.miretti@gmail.com>
2020-07-15 08:26:55 -0300

    :heavy_check_mark: test pwm init

--- aeropendulum/tests/test_websockets.py
+++ aeropendulum/tests/test_websockets.py
@@ -12,7 +12,7 @@ class WebsocketsTestCase(unittest.TestCase):
 
     def setUp(self):
         self.loop = asyncio.get_event_loop()
-        self.json_data = {"duty": 3.6}
+        self.json_data = {"duty": "0x0000"}
         self.message = json.dumps(self.json_data)
 
     async def connect(self, address, uri):
@@ -33,15 +33,14 @@ class WebsocketsTestCase(unittest.TestCase):
     async def a_test_classic_control_response(self):
         uri = "classic"
         websocket = await self.connect(AEROPENDULUM_IP_ADD, uri)
-
         #send to activate callback and receive
         await websocket.send(self.message)
         response = await websocket.recv()
         await websocket.close()
         parsed_response = json.loads(response)
 
-        self.assertIsInstance(parsed_response['angle'], float)
-        self.assertIsInstance(parsed_response['error'], float)
+        self.assertIsInstance(int(parsed_response['angle'], 16), int)
+        self.assertIsInstance(int(parsed_response['error'], 16), int)
 
     def test_classic_control_response(self):
         self.loop.run_until_complete(self.a_test_classic_control_response())
@@ -61,3 +60,14 @@ class WebsocketsTestCase(unittest.TestCase):
     def test_json_parsing(self):
         self.loop.run_until_complete(self.a_test_json_parsing())
 
+
+    async def a_test_pwm_start(self):
+        uri = "start_pwm"
+        websocket = await self.connect(AEROPENDULUM_IP_ADD, uri)
+
+        time.sleep(2)
+
+        await websocket.close()
+
+    def test_pwm_start(self):
+        self.loop.run_until_complete(self.a_test_pwm_start())
