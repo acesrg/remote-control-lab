09b9e7ef10f20c5d197edb1dc6bc036de5098303
MarcoMiretti <marco.miretti@gmail.com>
2020-07-19 11:10:44 -0300

    :hammer: refactor propeller constants

--- aeropendulum/esp/include/http_server.h
+++ aeropendulum/esp/include/http_server.h
@@ -1,9 +1,22 @@
 #include <log.h>
 
-#define LED_PIN 2
 
-#define ENCODER_PIN_A 12
-#define ENCODER_PIN_B 13
+/**
+ * Pinout for the ESP8266 D1 MINI
+ * https://user-images.githubusercontent.com/26353057/87876217-f3333a00-c9ac-11ea-9a11-893db497dc48.png
+ */
+#define LED_PIN             2
+
+#define ENCODER_PIN_A       12
+#define ENCODER_PIN_B       13
+
+#define PROPELLER_PWM_PIN   14
+
+/**
+ * Propeller default configs
+ * */
+#define DEFAULT_PROPELLER_PWM_FREQUENCY_HZ  100
+#define DEFAULT_PROPELLER_PWM_POWERON_DUTY  0x1C2A
 
 enum URI_TASK
 {
