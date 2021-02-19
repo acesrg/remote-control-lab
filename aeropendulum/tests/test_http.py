c8ca9186917baefa0edf0d828498077c2433bb50
marcomiretti <marco.miretti@gmail.com>
2021-02-19 08:49:07 -0300

    :nail_care: more accurate test name

--- aeropendulum/tests/test_http.py
+++ aeropendulum/tests/test_http.py
@@ -9,7 +9,7 @@ class HTTPTestCase(unittest.TestCase):
     def setUp(self):
         self.con = HTTPConnection(AEROPENDULUM_IP_ADD, port=80, timeout=5)
 
-    def test_uri_test(self):
+    def test_dummy_resource(self):
         VALUE = "hi_there"
         self.con.request("POST", "/test/resource?value=" + VALUE)
         self.assertEqual(self.con.getresponse().code, 202)
@@ -18,4 +18,3 @@ class HTTPTestCase(unittest.TestCase):
         res = self.con.getresponse()
         self.assertEqual(res.code, 200)
         self.assertEqual(res.read().decode('utf-8').splitlines()[0], VALUE)
-
