87ff2b25f9bb2e3f88619cb2ab0d674ef25c03b3
marcomiretti <marco.miretti@gmail.com>
2021-02-18 21:08:49 -0300

    :white_check_mark: add tests for HTTP connection

---
+++ aeropendulum/tests/test_http.py
@@ -0,0 +1,21 @@
+from esplab import HTTPConnection
+import unittest
+
+# TODO: this should be more automatic
+AEROPENDULUM_IP_ADD = "192.168.100.41"
+
+
+class HTTPTestCase(unittest.TestCase):
+    def setUp(self):
+        self.con = HTTPConnection(AEROPENDULUM_IP_ADD, port=80, timeout=5)
+
+    def test_uri_test(self):
+        VALUE = "hi_there"
+        self.con.request("POST", "/test/resource?value=" + VALUE)
+        self.assertEqual(self.con.getresponse().code, 202)
+
+        self.con.request("GET", "/test/resource")
+        res = self.con.getresponse()
+        self.assertEqual(res.code, 200)
+        self.assertEqual(res.read().decode('utf-8').splitlines()[0], VALUE)
+
